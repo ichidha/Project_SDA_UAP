4fdf4ad4f71c5a3f2c1f83a86e040ced628f9e46
NUR RAMADHANI <kasdhan43@gmail.com>
2025-06-15 11:44:47 +0700

    Update helper.py

--- Fiksasi/helper.py
+++ Fiksasi/helper.py
@@ -1,3 +1,4 @@
+#buat shadow teks
 def draw_shadow_text(canvas, x, y, text):
     for dx, dy, color in [(0, 4, "white"), (1, 3, "black"), (3, 0, "orange")]:
         canvas.create_text(x + dx, y + dy, text=text, font=("Forte", 68, "bold"), fill=color)
