a1cb08629047efbfaa611e4e32edbda26c07b9d3
NUR RAMADHANI <kasdhan43@gmail.com>
2025-06-15 11:45:53 +0700

    Update menu_page.py

--- Fiksasi/menu_page.py
+++ Fiksasi/menu_page.py
@@ -9,17 +9,21 @@ class MenuPage(tk.Frame):
         canvas.pack(fill="both", expand=True)
         canvas.create_image(0, 0, image=self.controller.bg2_image, anchor="nw")
 
+        #garis variasi
         for x in [275, 825]:
             for y in [230, 340]:
                 canvas.create_text(x, y, text="_____", font=("Forte", 68, "bold"), fill="white")
             for y in [245 ,325]:
                 canvas.create_text(x, y, text="____", font=("Forte", 68, "bold"), fill="white")
-        
+
+        #tombol projek
         project_btn = tk.Button(self, image=self.controller.btn_project, borderwidth=0,command=lambda: controller.show_frame("ProjectPage"), cursor="hand2")
         canvas.create_window(275, 325, window=project_btn)
 
+        #tombol team
         team_btn = tk.Button(self, image=self.controller.btn_team, borderwidth=0,command=lambda: controller.show_frame("TeamPage"), cursor="hand2")
         canvas.create_window(825, 325, window=team_btn)
 
+        #tombol back
         back_btn = tk.Button(self, image=self.controller.btn_back, borderwidth=0,command=lambda: controller.show_frame("WelcomePage"), cursor="hand2")
         canvas.create_window(895, 575, window=back_btn)
