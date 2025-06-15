7f7405c7fbbc5162f2536b64e41315a3ba14f024
NUR RAMADHANI <kasdhan43@gmail.com>
2025-06-15 11:52:05 +0700

    Update welcome_page.py

--- Fiksasi/welcome_page.py
+++ Fiksasi/welcome_page.py
@@ -11,12 +11,15 @@ class WelcomePage(tk.Frame):
         canvas.pack(fill="both", expand=True)
         canvas.create_image(0, 0, image=self.controller.bg_image, anchor="nw")
 
+        #shadow teks
         draw_shadow_text(canvas, 547, 100, "WELCOME")
         draw_shadow_text(canvas, 547, 175, "TO OUR")
         draw_shadow_text(canvas, 547, 250, "PROJECT")
 
+        #tombol start
         start_btn = tk.Button(self, image=self.controller.btn_start, borderwidth=0,command=lambda: controller.show_frame("MenuPage"), cursor="hand2")
         canvas.create_window(550, 420, window=start_btn)
 
+        #tombol exit
         exit_btn = tk.Button(self, image=self.controller.btn_exit, borderwidth=0,command=lambda: quit(), cursor="hand2")
         canvas.create_window(550, 500, window=exit_btn)
