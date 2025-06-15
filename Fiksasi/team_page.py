f5ae62836cb4aec77593a43c744f7820bfdecded
NUR RAMADHANI <kasdhan43@gmail.com>
2025-06-15 11:51:25 +0700

    Update team_page.py

--- Fiksasi/team_page.py
+++ Fiksasi/team_page.py
@@ -10,16 +10,22 @@ class TeamPage(tk.Frame):
         canvas.pack(fill="both", expand=True)
         canvas.create_image(0, 0, image=self.controller.bgAllName_image, anchor="nw")
 
+        #shadow teks
         draw_shadow_text(canvas, 547, 100, "OUR TEAM")
-        
+
+        #tombol back
         back_btn = tk.Button(self, image=self.controller.btn_back, borderwidth=0,command=lambda: controller.show_frame("MenuPage"), cursor="hand2")
         canvas.create_window(895, 575, window=back_btn)
 
+        #tombol profil Nur Ramadhani
         canvas.create_window(290, 230, window=tk.Button(self, image=self.controller.btn_nrd, borderwidth=0,
                                                         command=lambda: controller.show_frame("ProfilDhn"), cursor="hand2"))
+        #tombol profil Muthia Zhafira
         canvas.create_window(290, 330, window=tk.Button(self, image=self.controller.btn_muti, borderwidth=0,
                                                         command=lambda: controller.show_frame("ProfilMuti"), cursor="hand2"))
+        #tombol profil Viandra Thirdya Agsatri
         canvas.create_window(830, 230, window=tk.Button(self, image=self.controller.btn_via, borderwidth=0,
                                                         command=lambda: controller.show_frame("ProfilVia"), cursor="hand2"))
+        #tombol profil FX Novryandika W.
         canvas.create_window(830, 330, window=tk.Button(self, image=self.controller.btn_fx, borderwidth=0,
                                                         command=lambda: controller.show_frame("ProfilFx"), cursor="hand2"))
